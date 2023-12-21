b3805719695d3f1c3bfde54a9fec61800811e94f
Matheus Ribeiro <srdmatheus@gmail.com>
2023-12-21 16:24:41 -0300

    feat: set colors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-br">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} bg-background text-primary`}>
+        {children}
+      </body>
     </html>
   )
 }
