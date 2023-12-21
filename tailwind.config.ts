3e41d35ed611dcd9c00f9ca6779a11ec8582be01
Matheus Ribeiro <srdmatheus@gmail.com>
2023-12-21 16:24:25 -0300

    chore: extend colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,17 +4,19 @@ const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+      colors: {
+        accent: '#49D381',
+        orange: '#FFBB00',
+        background: '#121212',
+        primary: '#B1B1B1',
+        secondary: '#FFFFFF'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
 export default config
