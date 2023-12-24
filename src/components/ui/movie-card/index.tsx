c977e3799223985e9aad1428df8b60f015d54bf7
Matheus Ribeiro <srdmatheus@gmail.com>
2023-12-23 21:36:40 -0300

    feat: add <MovieCard />

--- src/components/ui/movie-card/index.tsx
+++ src/components/ui/movie-card/index.tsx
@@ -1,39 +1,4 @@
-import Image from 'next/image'
-import { Card } from '../card'
-import Link from 'next/link'
+import { MovieCard } from './movie-card'
+import { MovieCardSkeleton } from './skeleton'
 
-type MovieCardProps = {
-  title: string
-  releaseYear: string
-  genre: string
-  poster: string
-  url: string
-}
-
-export const MovieCard = ({
-  title,
-  releaseYear,
-  genre,
-  poster,
-  url
-}: MovieCardProps) => {
-  return (
-    <Link className="inline-flex" href={url}>
-      <Card.Root>
-        <Image
-          src={poster}
-          width={250}
-          height={375}
-          alt="name"
-          className="absolute top-0 right-0 left-0 hover:scale-105 transition-all duration-500"
-        />
-        <Card.Header>
-          <Card.Title>{title}</Card.Title>
-          <Card.Description>
-            {releaseYear} • {genre}
-          </Card.Description>
-        </Card.Header>
-      </Card.Root>
-    </Link>
-  )
-}
+export { MovieCard, MovieCardSkeleton }
---
+++ src/components/ui/movie-card/movie-card.tsx
@@ -0,0 +1,37 @@
+import Link from 'next/link'
+import Image from 'next/image'
+
+import { Card } from '../card'
+
+type MovieCardProps = {
+  title: string
+  releaseYear: string
+  genre: string
+  poster: string
+  url: string
+}
+
+export const MovieCard = ({
+  title,
+  releaseYear,
+  genre,
+  poster,
+  url
+}: MovieCardProps) => {
+  return (
+    <Link className="inline-flex" href={url}>
+      <Card.Root>
+        <div className="absolute left-0 right-0 top-0 h-[375px] w-[250px] transition-all duration-500 hover:scale-105">
+          <Image src={poster} fill alt={title} />
+        </div>
+
+        <Card.Header>
+          <Card.Title>{title}</Card.Title>
+          <Card.Description>
+            {releaseYear} • {genre}
+          </Card.Description>
+        </Card.Header>
+      </Card.Root>
+    </Link>
+  )
+}
