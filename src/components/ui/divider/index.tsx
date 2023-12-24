b317ce0285cbee46111dde5ae3ac65c89eeb96d9
Matheus Ribeiro <srdmatheus@gmail.com>
2023-12-24 19:07:12 -0300

    style: change border color

--- src/components/ui/divider/index.tsx
+++ src/components/ui/divider/index.tsx
@@ -6,7 +6,7 @@ type DividerProps = {
 export const Divider = ({ className }: DividerProps) => {
   return (
     <div className={cn('flex w-full py-4', className)}>
-      <div className={cn('flex w-full border-t border-primary/60')} />
+      <div className={cn('flex w-full border-t border-primary/50')} />
     </div>
   )
 }
