bf70680de72552fe149d7a9907ae28d080707e13
Matheus Ribeiro <srdmatheus@gmail.com>
2023-12-24 19:07:50 -0300

    style: add accent color to active link

--- src/components/navigation/navigation-item.tsx
+++ src/components/navigation/navigation-item.tsx
@@ -3,6 +3,7 @@
 import Link, { LinkProps } from 'next/link'
 
 import { cn } from '@/utils/cn'
+import { usePathname } from 'next/navigation'
 
 type NavigationItemProps = LinkProps & {
   className?: string
@@ -14,11 +15,14 @@ export const NavigationItem = ({
   href,
   className
 }: NavigationItemProps) => {
+  const pathname = usePathname()
+  const isActive = pathname === href.toString()
   return (
     <li>
       <Link
         className={cn(
           'flex h-full w-full gap-2 rounded-md border-l-4 border-transparent px-2 py-3 transition-all hover:border-accent hover:bg-primary/20 hover:text-accent',
+          isActive ? 'border-accent text-accent' : '',
           className
         )}
         href={href}
