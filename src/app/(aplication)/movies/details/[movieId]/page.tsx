f21d5557d8bf34297eababef05818d61ffec670f
Matheus Ribeiro <srdmatheus@gmail.com>
2023-12-25 03:07:15 -0300

    feat: add page movie details

--- src/app/(aplication)/movies/details/[movieId]/page.tsx
+++ src/app/(aplication)/movies/details/[movieId]/page.tsx
@@ -1,4 +1,19 @@
-import { getMovieDetails } from '@/functions/fetch-api'
+import {
+  getBackdrops,
+  getCredits,
+  getMovieDetails
+} from '@/functions/fetch-api'
+import { Banner } from '../../components/banner'
+import { BackButton } from '../../components/back-button'
+
+import { Divider } from '@/components/ui/divider'
+import { CastList } from '../../components/cast-list'
+import { Director } from '../../components/director'
+import { Backdrops } from '../../components/backdrops'
+import { Title } from '@/components/ui/title'
+import { BadgeList } from '../../components/badge-list'
+import { Details } from '../../components/details'
+import { HeaderDetails } from '../../components/header-details'
 
 type MovieDetailsProps = {
   params: {
@@ -6,7 +21,50 @@ type MovieDetailsProps = {
   }
 }
 export default async function MovieDetails({ params }: MovieDetailsProps) {
-  const data = await getMovieDetails({ id: params.movieId })
+  const { movieId } = params
 
-  return <div>{JSON.stringify(data)}</div>
+  const [movieDetails, credits, backdrops] = await Promise.all([
+    getMovieDetails({ id: movieId }),
+    getCredits({ id: movieId }),
+    getBackdrops({ id: movieId })
+  ])
+
+  return (
+    <main className="relative mt-4 flex w-full flex-col gap-4">
+      <Banner movieId={movieDetails.id} />
+      <BackButton className="absolute left-8 top-8 z-20" />
+
+      <HeaderDetails data={movieDetails} />
+
+      <BadgeList movieId={movieId} />
+
+      <Divider />
+
+      <section>
+        <Title variant="h3">Detalhes</Title>
+        <Details data={movieDetails} />
+      </section>
+
+      <Divider />
+
+      <section>
+        <Title variant="h3">Diretor (a)</Title>
+        <Director data={credits} />
+      </section>
+
+      <Divider />
+
+      <section>
+        <Title variant="h3">Atores</Title>
+        <CastList data={credits} />
+      </section>
+
+      <Divider />
+
+      <section>
+        <Title variant="h3">Galeria</Title>
+        <Backdrops data={backdrops} />
+      </section>
+    </main>
+  )
 }
