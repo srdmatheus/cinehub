6c3ac5323bf4b62658d68e2e6e1eb0b1f48d01a1
Matheus Ribeiro <srdmatheus@gmail.com>
2023-12-25 03:13:44 -0300

    fix: title app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import '@/styles/globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'CineHub',
+  title: 'Cinehub',
   description:
     'O CineHub é uma fonte valiosa para os amantes do cinema e da televisão que desejam descobrir, explorar e acompanhar os títulos mais populares, os ranqueados e os lançamentos mais recentes.'
 }
