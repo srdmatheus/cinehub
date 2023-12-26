d659870362954cd3272b1527a20a13f52a25fa6d
Matheus Ribeiro <srdmatheus@gmail.com>
2023-12-26 19:42:27 -0300

    feat: add MovieList to page

--- src/app/(aplication)/movies/popular/page.tsx
+++ src/app/(aplication)/movies/popular/page.tsx
@@ -1,3 +1,20 @@
-export default function Popular() {
-  return <div>Popular</div>
+import { Title } from '@/components/ui/title'
+import { MovieListSkeleton } from '../components/movie-list/movie-list-skeleton'
+import { Suspense } from 'react'
+import { MovieList } from '../components/movie-list'
+import { getMoviesByCategory } from '@/functions/fetch-api'
+
+export default async function Popular() {
+  const movies = await getMoviesByCategory({ category: 'popular' })
+
+  return (
+    <>
+      <Title variant="h2" className="mb-4">
+        Em alta
+      </Title>
+      <Suspense fallback={<MovieListSkeleton />}>
+        <MovieList movies={movies} />
+      </Suspense>
+    </>
+  )
 }
