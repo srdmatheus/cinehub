41d8e9fa35330d21d66616a1e77eba39340ddb05
Matheus Ribeiro <srdmatheus@gmail.com>
2023-12-26 09:59:03 -0300

    refact: simplify code

--- src/app/(aplication)/movies/components/movie-list/movie-list-skeleton.tsx
+++ src/app/(aplication)/movies/components/movie-list/movie-list-skeleton.tsx
@@ -3,16 +3,9 @@ import { MovieCardSkeleton } from '@/components/ui/movie-card'
 export const MovieListSkeleton = () => {
   return (
     <div className="flex gap-6">
-      {
-        <>
-          <MovieCardSkeleton />
-          <MovieCardSkeleton />
-          <MovieCardSkeleton />
-          <MovieCardSkeleton />
-          <MovieCardSkeleton />
-          <MovieCardSkeleton />
-        </>
-      }
+      {[...new Array(12)].map((_, i) => (
+        <MovieCardSkeleton key={i} />
+      ))}
     </div>
   )
 }
