e0bc6bea5e69db9f3230a87405b0768471ddb0cd
Matheus Ribeiro <srdmatheus@gmail.com>
2023-12-26 19:42:22 -0300

    feat: add MovieList to page

--- src/app/(aplication)/movies/top-rated/page.tsx
+++ src/app/(aplication)/movies/top-rated/page.tsx
@@ -1,3 +1,20 @@
-export default function TopRated() {
-  return <div>top rated</div>
+import { Title } from '@/components/ui/title'
+import { Suspense } from 'react'
+import { MovieListSkeleton } from '../components/movie-list/movie-list-skeleton'
+import { MovieList } from '../components/movie-list'
+import { getMoviesByCategory } from '@/functions/fetch-api'
+
+export default async function TopRated() {
+  const movies = await getMoviesByCategory({ category: 'top_rated' })
+
+  return (
+    <>
+      <Title variant="h1" className="mb-4">
+        Mais votados
+      </Title>
+      <Suspense fallback={<MovieListSkeleton />}>
+        <MovieList movies={movies} />
+      </Suspense>
+    </>
+  )
 }
