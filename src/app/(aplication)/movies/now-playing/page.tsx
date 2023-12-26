a226d7775112e65d502c5236daa4e596cb61c433
Matheus Ribeiro <srdmatheus@gmail.com>
2023-12-26 19:42:33 -0300

    feat: add MovieList to page

--- src/app/(aplication)/movies/now-playing/page.tsx
+++ src/app/(aplication)/movies/now-playing/page.tsx
@@ -1,3 +1,20 @@
-export default function NowPlaying() {
-  return <div>Now Playing</div>
+import { Title } from '@/components/ui/title'
+import { Suspense } from 'react'
+import { MovieList } from '../components/movie-list'
+import { MovieListSkeleton } from '../components/movie-list/movie-list-skeleton'
+import { getMoviesByCategory } from '@/functions/fetch-api'
+
+export default async function NowPlaying() {
+  const movies = await getMoviesByCategory({ category: 'now_playing' })
+
+  return (
+    <>
+      <Title variant="h2" className="mb-4">
+        Nos cinemas
+      </Title>
+      <Suspense fallback={<MovieListSkeleton />}>
+        <MovieList movies={movies} />
+      </Suspense>
+    </>
+  )
 }
