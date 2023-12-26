32a474675959fc1c4efec751da9cc4b6775c1b6f
Matheus Ribeiro <srdmatheus@gmail.com>
2023-12-26 19:44:53 -0300

    refact: add search to homepage

--- src/app/(aplication)/page.tsx
+++ src/app/(aplication)/page.tsx
@@ -1,38 +1,17 @@
-import { Suspense } from 'react'
-import { MovieListSkeleton } from './movies/components/movie-list/movie-list-skeleton'
-import { MovieList } from './movies/components/movie-list'
+import { Title } from '@/components/ui/title'
+import { Search } from './movies/components/search'
 
 export default function Home() {
   return (
-    <main className="flex flex-col gap-8">
-      <section>
-        <h2 className="mb-2 text-3xl font-semibold tracking-tighter">
-          Nos cinemas
-        </h2>
-        <Suspense fallback={<MovieListSkeleton />}>
-          <MovieList category="now_playing" />
-        </Suspense>
-      </section>
+    <>
+      <Title variant="h1" className="mb-0.5 leading-none">
+        Discover
+      </Title>
+      <p className="mb-6 text-base italic">
+        Pesquise qualquer nome, encontre qualquer filme!
+      </p>
 
-      <section>
-        <h2 className="mb-2 text-3xl font-semibold tracking-tighter">
-          Em alta
-        </h2>
-        <Suspense fallback={<MovieListSkeleton />}>
-          <MovieList category="popular" />
-        </Suspense>
-      </section>
-
-      <section>
-        <div className="flex items-start gap-4">
-          <h2 className="mb-2 text-3xl font-semibold tracking-tighter">
-            Mais votados
-          </h2>
-        </div>
-        <Suspense fallback={<MovieListSkeleton />}>
-          <MovieList category="top_rated" />
-        </Suspense>
-      </section>
-    </main>
+      <Search />
+    </>
   )
 }
