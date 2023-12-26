94bc8f4a232e509119bae320fc834a8d675ce6a1
Matheus Ribeiro <srdmatheus@gmail.com>
2023-12-26 19:41:32 -0300

    style: add gap for icon

--- src/components/ui/button/index.tsx
+++ src/components/ui/button/index.tsx
@@ -6,7 +6,7 @@ import { Slot } from '@radix-ui/react-slot'
 import { cn } from '@/utils/cn'
 
 export const buttonVariants = cva(
-  'px-4 py-2 inline-flex items-center justify-center whitespace-nowrap rounded-md font-medium transition-all disabled:pointer-events-none disabled:opacity-50',
+  'px-4 py-2 inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md font-medium transition-all disabled:pointer-events-none disabled:opacity-50',
   {
     variants: {
       variant: {
