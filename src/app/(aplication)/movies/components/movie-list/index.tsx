4c8c276e82cfe62ce1a79b03dc47cab7db8bb800
Matheus Ribeiro <srdmatheus@gmail.com>
2023-12-26 19:44:01 -0300

    refact: change necessary prop

--- src/app/(aplication)/movies/components/movie-list/index.tsx
+++ src/app/(aplication)/movies/components/movie-list/index.tsx
@@ -1,23 +1,28 @@
-import { filterMovieGenres } from '@/app/functions/filterMovieGenres'
-import { MovieCard } from '@/components/ui/movie-card'
+import { filterMovieGenres } from '@/functions/filterMovieGenres'
+import { MovieCard, MovieCardSkeleton } from '@/components/ui/movie-card'
+import { Movie } from '@/types/movies'
 
-import { getMoviesByCategory } from '@/functions/fetch-api'
-import { MovieListCategory } from '@/types/movies'
+type MovieListProps = {
+  movies: Movie[]
+}
 
-export const MovieList = async ({ category }: MovieListCategory) => {
-  const movies = await getMoviesByCategory({ category })
-
+export const MovieList = ({ movies }: MovieListProps) => {
   return (
     <div className="flex flex-wrap justify-between gap-8">
       {movies.map((movie) => (
-        <MovieCard
-          key={movie.id}
-          url={`/movies/details/${movie.id}`}
-          title={movie.title}
-          genre={filterMovieGenres(movie).join(', ')}
-          releaseYear={movie.release_date.slice(0, 4)}
-          poster={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-        />
+        <div key={movie.id}>
+          {movie.poster_path ? (
+            <MovieCard
+              url={`/movies/details/${movie.id}`}
+              title={movie.title}
+              genre={filterMovieGenres(movie).join(', ')}
+              releaseYear={movie.release_date.slice(0, 4)}
+              poster={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+            />
+          ) : (
+            <MovieCardSkeleton />
+          )}
+        </div>
       ))}
     </div>
   )
