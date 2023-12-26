80088da03c0bf947082525059c6922c101b0a2ee
Matheus Ribeiro <srdmatheus@gmail.com>
2023-12-25 23:29:58 -0300

    style: change display movies

--- src/app/(aplication)/movies/components/movie-list/index.tsx
+++ src/app/(aplication)/movies/components/movie-list/index.tsx
@@ -1,4 +1,6 @@
+import { filterMovieGenres } from '@/app/functions/filterMovieGenres'
 import { MovieCard } from '@/components/ui/movie-card'
+
 import { getMoviesByCategory } from '@/functions/fetch-api'
 import { MovieListCategory } from '@/types/movies'
 
@@ -6,13 +8,13 @@ export const MovieList = async ({ category }: MovieListCategory) => {
   const movies = await getMoviesByCategory({ category })
 
   return (
-    <div className="grid grid-cols-[repeat(auto-fit,_minmax(250px,_3fr))] items-start justify-start gap-4">
+    <div className="flex flex-wrap justify-between gap-8">
       {movies.map((movie) => (
         <MovieCard
           key={movie.id}
           url={`/movies/details/${movie.id}`}
           title={movie.title}
-          genre={movie.genre_ids.toString()}
+          genre={filterMovieGenres(movie).join(', ')}
           releaseYear={movie.release_date.slice(0, 4)}
           poster={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
         />
