7dcc1c22177dc618079841c6e3c761a4c16a6ed0
Matheus Ribeiro <srdmatheus@gmail.com>
2023-12-26 19:43:27 -0300

    style: change skeleton gap

--- src/app/(aplication)/movies/components/movie-list/movie-list-skeleton.tsx
+++ src/app/(aplication)/movies/components/movie-list/movie-list-skeleton.tsx
@@ -2,7 +2,7 @@ import { MovieCardSkeleton } from '@/components/ui/movie-card'
 
 export const MovieListSkeleton = () => {
   return (
-    <div className="flex gap-6">
+    <div className="flex flex-wrap justify-between gap-8">
       {[...new Array(12)].map((_, i) => (
         <MovieCardSkeleton key={i} />
       ))}
